290900ae0acf08829480a4f1a75749546fc01bd0
Matheus Hubert <matheushubert@gmail.com>
2024-01-31 12:23:00 +0100

    desafio completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "654321", modelo: "Modelo 2", imei: "2222222", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Games");
+
+
